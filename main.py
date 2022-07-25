31272835a45501c31e6bf6a2f049c1e2cda6e7cb
Sarah Burne James <sarahmbj@gmail.com>
2022-07-25 21:56:16 +0100

    rewrote insert_spaces() to enable max spaces

--- main.py
+++ main.py
@@ -13,16 +13,31 @@ def leave_only_consonants(lowercased_phrase):
 
 
 def insert_spaces(phrase):
-    #TODO: at some point, modify to limit number of spaces based on string length
     phrase = list(phrase)
-    for i in range(len(phrase)-1):
-        while random.randrange(2):
+    spaces = list(range(len(phrase)))
+    random.shuffle(spaces)
+
+    #truncate to create max spaces
+    max_spaces = int(len(phrase)/2)
+    spaces = spaces[0:max_spaces]
+
+    #loop through space indexes and randomly delete some (to vary number of spaces)
+    selected_spaces = []
+    for i in range(len(spaces)-1):
+        coin_flip = random.randrange(2)
+        if coin_flip == True:
+            selected_spaces.append(spaces[i])
+
+    #add the spaces to the phrase
+    for i in selected_spaces:
             phrase[i] = phrase[i] + ' '
     phrase_with_spaces = ''.join(phrase)
 
+    #find and remove consecutive spaces
     multiple_spaces = re.compile(r'\s\s+')
     phrase_with_spaces = re.sub(multiple_spaces, " ", phrase_with_spaces)
     return phrase_with_spaces
+
 
 def create_questions_from_answers(answer):
         question = answer.lower()
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import unittest
+from unittest import mock
 import main
+import random
+
 
 class TestStringManipulation(unittest.TestCase):
 
@@ -9,11 +12,15 @@ class TestStringManipulation(unittest.TestCase):
         self.assertEqual(main.leave_only_consonants('a hard day\'s night'), 'hrddysnght', "Should be hrddysnght")
 
     def test_insert_spaces(self):
-        #check input and output are not the same
-        self.assertNotEqual(main.insert_spaces('bbyrd'), 'bbyrd', "string should always be modified")
-        #check the output contains spaces
-        self.assertIn(" ", main.insert_spaces("hrddysnght"), "should contain at least one space")
-        #TODO: how else can we test this, given the randomness?
+        #check there are no double (or more) spaces
+        self.assertNotIn("  ", main.insert_spaces("hrddysnght"), "should not contain multiple spaces in a row")
+
+        #TODO: how else can we test this (and create_questions_from_answers), given the randomness? 
+        # Some of these tests may pass sometimes and fail others, based on the random placement of the spaces
+        # Need to add mocking for random numbers
+
+    # def test_create_questions_from_answers():
+    #     pass
 
 if __name__ == '__main__':
     unittest.main()
